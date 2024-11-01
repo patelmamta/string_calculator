93a16527f1a33f7758e44e3a9d2f86430c619ffb
patelmamta <patelmamta0492@gmail.com>
2024-11-01 18:19:19 +0530

    Test case number 5 - Allow newline between two numbers and with more than one digit number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,8 +14,12 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1,5")).to eq(6)
     end
 
-    it "handles newlines between numbers" do
+    it "handles newline between numbers" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it "allow newline between two numbers and with two digit numbers" do
+      expect(StringCalculator.add("11\n2,13")).to eq(26)
     end
   end
 end
