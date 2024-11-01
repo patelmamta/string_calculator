ee2da73e92f5ede13b2cdb09143468976c97fe60
patelmamta <patelmamta0492@gmail.com>
2024-11-01 17:59:48 +0530

    Add test case number 2 - returns string number to integer number

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
 
   ### class method to add string numbers
   def self.add(str)
-    return 0 if str.empty?
+    return 0 if str.empty? # empty string return 0
+    str.to_i # convert string number to integer
   end 
 end
