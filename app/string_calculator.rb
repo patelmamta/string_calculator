3cb3e59aad8542d8c6080e421ba4a5768b2257fc
patelmamta <patelmamta0492@gmail.com>
2024-11-01 18:08:06 +0530

    Test case number 3 - sum of multiple numbers in a single string

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -3,6 +3,6 @@ class StringCalculator
   ### class method to add string numbers
   def self.add(str)
     return 0 if str.empty? # empty string return 0
-    str.to_i # convert string number to integer
+    str.scan(/\d+/).map(&:to_i).sum # scan numbers from string and sum the digital numbers.
   end 
 end
